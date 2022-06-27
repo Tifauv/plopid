ac2987298b66d96146a25b29afc45fd568fadbe9
Olivier Serve <olivier.serve@atos.net>
2022-06-27 18:33:34 +0200

    login/pwd: validation du token session

--- src/web/authn/pwd.rs
+++ src/web/authn/pwd.rs
@@ -1,6 +1,10 @@
 use rocket::post;
+use rocket::Responder;
+use rocket_dyn_templates::Template;
 use rocket::form::{Form, FromForm, Strict};
-use rocket::http::{Status, ContentType};
+use jsonwebtoken::{Algorithm, DecodingKey, Validation};
+use crate::jwt::session;
+use crate::web::error;
 
 
 #[derive(FromForm)]
@@ -10,12 +14,95 @@ pub struct AuthnForm<'r> {
 }
 
 
-#[post("/login_pwd", data="<p_authn>")]
-pub fn login_pwd(p_authn: Form<Strict<AuthnForm<'_>>>) -> (Status, (ContentType, String)) {
-	if p_authn.u_login.eq("ptramo") && p_authn.u_password.eq("plopplopplop") {
-		(Status::Ok, (ContentType::HTML, "Yay! Welcome to Pierre TRAMO, our great J2EE Lead Architect!".to_string()))
-	}
-	else {
-		(Status::Unauthorized, (ContentType::HTML, "Boo! We not know you!".to_string())) 
-	}
+#[derive(serde::Serialize)]
+struct AuthnSuccessContext<'a> {
+    page_name: &'a str,
+    user_name: &'a str,
+    session_jwt: Option<String>,
+}
+
+#[derive(serde::Serialize)]
+struct AuthnFailureContext<'a> {
+    page_name: &'a str,
+    session_jwt: Option<String>,
+}
+
+#[derive(Responder)]
+pub enum PwdFormResponse {
+	#[response(status = 200)]
+	AuthnSuccess(Template),
+	#[response(status = 401)]
+	AuthnFailure(Template),
+	#[response(status = 400)]
+	BadRequest(Template),
+	#[response(status = 500)]
+	ServerError(Template),
+}
+
+
+#[post("/login_pwd?<session>", data="<p_authn>")]
+pub fn login_pwd(session: Option<String>, p_authn: Form<Strict<AuthnForm<'_>>>) -> PwdFormResponse {
+    // Check the session JWT
+    match session {
+        Some(t) => {
+			// TODO fetch from configuration
+			let cfg_jwt_internal_key   = b"secret";
+			let cfg_jwt_oidc_issuer    = "https://localhost:8000/oidc".to_string();
+			let cfg_jwt_authn_audience = "https://localhost:8000/authn".to_string();
+
+			let mut validation = Validation::new(Algorithm::HS256);
+			validation.set_required_spec_claims(&["exp", "aud", "iss"]);
+			validation.set_audience(&[cfg_jwt_authn_audience]);
+			validation.set_issuer(&[cfg_jwt_oidc_issuer]);
+			
+			match jsonwebtoken::decode::<session::OIDCSessionClaims>(
+                &t,
+                &DecodingKey::from_secret(cfg_jwt_internal_key),
+                &validation) {
+                Ok(_) => {
+					if p_authn.u_login.eq("ptramo") && p_authn.u_password.eq("plopplopplop") {
+						PwdFormResponse::AuthnSuccess(
+							Template::render("authn_success", &AuthnSuccessContext {
+								page_name: "Authentication succeeded",
+								user_name: "Pierre TRAMO",
+								session_jwt: Some(t),
+							})
+						)
+					}
+					else {
+						PwdFormResponse::AuthnFailure(
+							Template::render("authn_failure", &AuthnFailureContext {
+								page_name: "Authentication failed",
+								session_jwt: Some(t),
+							})
+						)
+					}
+                },
+                Err(e) => PwdFormResponse::BadRequest(error::error_page(
+                    400,
+                    "Failed to decode the JWT token",
+                    format!("{}", e).as_str()))
+            }
+        },
+        None => {
+			if p_authn.u_login.eq("ptramo") && p_authn.u_password.eq("plopplopplop") {
+				PwdFormResponse::AuthnSuccess(
+					Template::render("authn_success", &AuthnSuccessContext {
+						page_name: "Authentication succeeded",
+						user_name: "Pierre TRAMO",
+						session_jwt: None,
+					})
+				)
+			}
+			else {
+				PwdFormResponse::AuthnFailure(
+					Template::render("authn_failure", &AuthnFailureContext {
+						page_name: "Authentication failed",
+						session_jwt: None,
+					})
+				)
+			}
+        }
+    }
+
 }
